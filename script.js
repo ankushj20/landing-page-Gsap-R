92dcf77f24cb66daa136aeae7e063fafd8d844f7
unknown <ankushjaiswal7482@gmail.com>
2024-01-08 22:33:08 +0530

    landing page complete

---
+++ script.js
@@ -0,0 +1,29 @@
+gsap.from("#left h1, #abbt h4, #nav h2",{
+    y:-50,
+    opacity:0,
+    stagger:0.3,
+    delay:1,
+    scale:0.5
+
+
+})
+gsap.from("#lft h1",{
+    x:-200,
+    opacity:0,
+    stagger:0.2,
+    delay:1.5,
+})
+gsap.from("#btm h4",{
+    y:400,
+    opacity:0,
+    delay:1,
+
+})
+gsap.from("#rgt img",{
+    scale:0,
+    duration:1,
+    delay:1,
+    opactiy:-1,
+
+    
+})
